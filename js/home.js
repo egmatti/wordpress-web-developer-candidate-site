84da97a9d5e1d7b1af2a798c5dc67429f5b86d04
Emily Mattison <egmatti@gmail.com>
2017-09-22 15:31:04 -0400

    Started adding the floor plans slider

--- js/home.js
+++ js/home.js
@@ -1,3 +1,32 @@
 // ==========================================
 // HOME PAGE
 // ==========================================
+
+// FLOOR PLANS SLIDER
+
+$(".floor-plans-slider-for--desktop").slick({
+ arrows: true,
+ dots: true,
+ infinite: true,
+ slidesToScroll: 1,
+ slidesToShow: 4,
+ vertical: false
+});
+
+$(".floor-plans-slider-for--tablet").slick({
+ arrows: true,
+ dots: true,
+ infinite: true,
+ slidesToScroll: 1,
+ slidesToShow: 2,
+ vertical: false
+});
+
+$(".floor-plans-slider-for--mobile").slick({
+ arrows: true,
+ dots: true,
+ infinite: true,
+ slidesToScroll: 1,
+ slidesToShow: 1,
+ vertical: false
+});
