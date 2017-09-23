e18c4223b5f5865a8cd11995d175c0a2bd8704d0
Emily Mattison <egmatti@gmail.com>
2017-09-22 23:47:53 -0400

    Styled floor plan slider posts

--- js/home.js
+++ js/home.js
@@ -4,7 +4,7 @@
 
 // FLOOR PLANS SLIDER
 
-$(".floor-plans-slider-for--desktop").slick({
+$(".floor-plans-slider--desktop").slick({
  arrows: true,
  dots: true,
  infinite: true,
@@ -13,7 +13,7 @@ $(".floor-plans-slider-for--desktop").slick({
  vertical: false
 });
 
-$(".floor-plans-slider-for--tablet").slick({
+$(".floor-plans-slider--tablet").slick({
  arrows: true,
  dots: true,
  infinite: true,
@@ -22,7 +22,7 @@ $(".floor-plans-slider-for--tablet").slick({
  vertical: false
 });
 
-$(".floor-plans-slider-for--mobile").slick({
+$(".floor-plans-slider--mobile").slick({
  arrows: true,
  dots: true,
  infinite: true,
@@ -30,3 +30,13 @@ $(".floor-plans-slider-for--mobile").slick({
  slidesToShow: 1,
  vertical: false
 });
+
+
+// FLOOR PLANS IMAGE CONTAINER HEIGHT
+
+var floorPlansPostHeight = $(".floor-plans-post").height();
+var floorPlansTitleContainerHeight = $(".floor-plans-post__title-container").outerHeight(true);
+var floorPlansDetailsContainerHeight = $(".floor-plans-post__details-container").outerHeight();
+var floorPlansImageContainerHeight = floorPlansPostHeight - floorPlansTitleContainerHeight - floorPlansDetailsContainerHeight;
+
+$(".floor-plans-post__image-container").css({"height": floorPlansImageContainerHeight});
