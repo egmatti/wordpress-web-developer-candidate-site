937134429acf939abce59990aa42c0a0232fda3b
Emily Mattison <egmatti@gmail.com>
2017-09-24 16:18:15 -0400

    Added mobile media queries

--- js/home.js
+++ js/home.js
@@ -34,9 +34,25 @@ $(".floor-plans-slider--mobile").slick({
 
 // FLOOR PLANS IMAGE CONTAINER HEIGHT
 
-var floorPlansPostHeight = $(".floor-plans-post").height();
-var floorPlansTitleContainerHeight = $(".floor-plans-post__title-container").outerHeight(true);
-var floorPlansDetailsContainerHeight = $(".floor-plans-post__details-container").outerHeight();
-var floorPlansImageContainerHeight = floorPlansPostHeight - floorPlansTitleContainerHeight - floorPlansDetailsContainerHeight;
+var floorPlansPostHeightDesktop = $(".floor-plans-slider--desktop .floor-plans-post").height();
+var floorPlansTitleContainerHeightDesktop = $(".floor-plans-slider--desktop .floor-plans-post__title-container").outerHeight(true);
+var floorPlansDetailsContainerHeightDesktop = $(".floor-plans-slider--desktop .floor-plans-post__details-container").outerHeight();
+var floorPlansImageContainerHeightDesktop = floorPlansPostHeightDesktop - floorPlansTitleContainerHeightDesktop - floorPlansDetailsContainerHeightDesktop;
 
-$(".floor-plans-post__image-container").css({"height": floorPlansImageContainerHeight});
+$(".floor-plans-slider--desktop .floor-plans-post__image-container").css({"height": floorPlansImageContainerHeightDesktop});
+
+
+var floorPlansPostHeightTablet = $(".floor-plans-slider--tablet .floor-plans-post").height();
+var floorPlansTitleContainerHeightTablet = $(".floor-plans-slider--tablet .floor-plans-post__title-container").outerHeight(true);
+var floorPlansDetailsContainerHeightTablet = $(".floor-plans-slider--tablet .floor-plans-post__details-container").outerHeight();
+var floorPlansImageContainerHeightTablet = floorPlansPostHeightTablet - floorPlansTitleContainerHeightTablet - floorPlansDetailsContainerHeightTablet;
+
+$(".floor-plans-slider--tablet .floor-plans-post__image-container").css({"height": floorPlansImageContainerHeightTablet});
+
+
+var floorPlansPostHeightMobile = $(".floor-plans-slider--mobile .floor-plans-post").height();
+var floorPlansTitleContainerHeightMobile = $(".floor-plans-slider--mobile .floor-plans-post__title-container").outerHeight(true);
+var floorPlansDetailsContainerHeightMobile = $(".floor-plans-slider--mobile .floor-plans-post__details-container").outerHeight();
+var floorPlansImageContainerHeightMobile = floorPlansPostHeightMobile - floorPlansTitleContainerHeightMobile - floorPlansDetailsContainerHeightMobile;
+
+$(".floor-plans-slider--mobile .floor-plans-post__image-container").css({"height": floorPlansImageContainerHeightMobile});
